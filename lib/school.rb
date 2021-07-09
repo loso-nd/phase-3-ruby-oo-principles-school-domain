49b5b4c3d8d3bdb6435d517426d29dfc3f5bab17
Nahum <losond8@gmail.com>
2021-07-09 01:00:48 -0400

    commit tree 8687d4734bcd93eec4099a28ffe8d91b5a1c65bf

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
 # code here!
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @name = name
+        @grade = grade
+
+    end
+binding.pry
+end
